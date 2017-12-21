f6c2609c59f8c17b410625e1c14976b30e4f24f1
AlejandroJG <ajaimezg@hotmail.com>
2017-12-21 15:34:58 +0100

    Projecte en una nova versió, problemes pasar a fragments

---
+++ app/src/main/java/com/example/danielmartinezg1/ligasantander/NavigationDrawerActivity.java
@@ -0,0 +1,133 @@
+package com.example.danielmartinezg1.ligasantander;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static com.example.danielmartinezg1.ligasantander.R.id.drawer_layout;
+
+public class NavigationDrawerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.side_nav_bar);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        drawer = (DrawerLayout) findViewById(drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        switch (menuItem.getItemId()) {
+            case R.id.partidos:
+                menuItem.setChecked(true);
+                setFragment(0);
+                drawer.closeDrawer(GravityCompat.START);
+                Log.i("Dani","clicado");
+                return true;
+            case R.id.calendario:
+                menuItem.setChecked(true);
+                setFragment(1);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+
+
+        }
+        return true;
+
+
+    }
+
+
+    public void setFragment(int position) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+            case 0:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                BlankFragment BlankFragment = new BlankFragment();
+                fragmentTransaction.replace(R.id.fragment, BlankFragment);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                CalendarFragment calendarFragment = new CalendarFragment();
+                fragmentTransaction.replace(R.id.fragment, calendarFragment);
+                fragmentTransaction.commit();
+                break;
+
+        }
+    }
+
+}
+
